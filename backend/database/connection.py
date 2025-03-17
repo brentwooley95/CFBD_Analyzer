46e0b686d644d2e00e27de584ce4b032ef8c1f38
brentwooley95 <138704843+brentwooley95@users.noreply.github.com>
2025-03-17 12:29:39 -0500

    Update connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -4,4 +4,8 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:PASSWORD@localhost:PORT/DATABASE_NAME")
 
 async def connect_to_db():
-    return await asyncpg.create_pool(DATABASE_URL)
+    return await asyncpg.create_pool(
+        DATABASE_URL,
+        min_size=1,  # Minimum 1 connection
+        max_size=10  # Maximum 10 connections at a time
+    )
