008921020b4010ec32a95d42b75d52ac4e324f48
brentwooley95 <138704843+brentwooley95@users.noreply.github.com>
2025-03-17 12:45:31 -0500

    Create TeamDrop.js

---
+++ frontend/src/components/TeamDrop.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const TeamDrop = ({ teams, selectedTeam, handleChange }) => {
+    return (
+        <div className="card shadow-sm mb-4">
+            <div className="card-body">
+                <h4 className="card-title text-center">Select Team</h4>
+                <div className="mb-3">
+                    <label className="fw-bold">Team</label>
+                    <select name="team" value={selectedTeam} onChange={handleChange} className="form-select">
+                        <option value="">Select Team</option>
+                        {teams.map((team) => (
+                            <option key={team} value={team}>{team}</option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TeamDrop;
+
