99e64f83213fd381072e2a6985a24febbb8dfb89
brentwooley95 <138704843+brentwooley95@users.noreply.github.com>
2025-04-22 22:08:43 -0500

    Update supabaseClient.js

--- frontend/src/services/supabaseClient.js
+++ frontend/src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co';
-const supabaseAnonKey = 'YOUR_ANON_KEY';
+const supabaseUrl = 'https://rsmuaszmgoalenolgzyb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzbXVhc3ptZ29hbGVub2xnenliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NTQxMDIsImV4cCI6MjA2MDIzMDEwMn0.JENnfBnBKwn-7oPTjcyRiDfjtn0JIW69Pfm1JD4_Uf0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
