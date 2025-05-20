028154a15d8d86208a9f7322798ab4e359da8be1
brentwooley95 <138704843+brentwooley95@users.noreply.github.com>
2025-05-20 19:15:53 +0000

    Create colorHelpers.js

---
+++ frontend/src/styles/colorHelpers.js
@@ -0,0 +1,32 @@
+// utils/colorHelpers.js
+export const getColorClass = (value, isSuccessRate = false, reverseScale = false) => {
+    let thresholds;
+
+    if (isSuccessRate) {
+        thresholds = reverseScale
+            ? [
+                  { limit: 0.3619, color: "bg-success" },
+                  { limit: 0.3989, color: "bg-success-subtle" },
+                  { limit: 0.4449, color: "bg-warning-subtle" },
+                  { limit: 0.4799, color: "bg-danger-subtle" },
+                  { limit: Infinity, color: "bg-danger" }
+              ]
+            : [
+                  { limit: 0.3619, color: "bg-danger" },
+                  { limit: 0.3989, color: "bg-danger-subtle" },
+                  { limit: 0.4449, color: "bg-warning-subtle" },
+                  { limit: 0.4799, color: "bg-success-subtle" },
+                  { limit: Infinity, color: "bg-success" }
+              ];
+    } else {
+        thresholds = [
+            { limit: 32.99, color: "bg-danger" },
+            { limit: 41.99, color: "bg-danger-subtle" },
+            { limit: 56.99, color: "bg-warning-subtle" },
+            { limit: 69.99, color: "bg-success-subtle" },
+            { limit: Infinity, color: "bg-success" }
+        ];
+    }
+
+    return thresholds.find(t => value < t.limit)?.color || "";
+};
