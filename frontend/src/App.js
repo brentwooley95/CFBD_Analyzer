b88a59d96adf7d3abd10920a6a29f16d386e7d36
brentwooley95 <138704843+brentwooley95@users.noreply.github.com>
2025-05-27 19:00:09 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,15 +13,31 @@ const App = () => {
   return (
     <Router>
       {/* Navigation Bar */}
-      <nav className='navbar navbar-dark bg-primary'>
+      <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
         <div className='container-fluid'>
           <Link className='navbar-brand' to='/'>CFB Matchup Tool</Link>
-          <div>
-            <Link className='btn btn-light me-2' to='/'>Home</Link>
-            <Link className='btn btn-light me-2' to='/about/'>About</Link>
-            <Link className='btn btn-light me-2' to='/team/'>Team View</Link>
-            <Link className='btn btn-light me-2' to='/team-comparison/'>Compare Team Seasons</Link>
-            <Link className='btn btn-light me-2' to='/games/'>Game View</Link>
+
+          {/* toggle button */}
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <div className='navbar-nav ms-auto'>
+              <Link className='nav-link text-white' to='/'>Home</Link>
+              <Link className='nav-link text-white' to='/about/'>About</Link>
+              <Link className='nav-link text-white' to='/team/'>Team View</Link>
+              <Link className='nav-link text-white' to='/team-comparison/'>Compare Team Seasons</Link>
+              <Link className='nav-link text-white' to='/games/'>Game View</Link>
+            </div>
           </div>
         </div>
       </nav>
